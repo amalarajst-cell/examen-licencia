660cd7f6560c964d3897814d675392570076dc80
amalarajst@gmail.com <respetealmotociclista@gmail.com>
2026-03-16 11:13:17 -0300

    Configure deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/REPLACE_WITH_REPO_NAME/',
+  base: '/examen-licencia/',
 })
